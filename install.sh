cb029de43f4792e2cfb05e3103aeed2c138b0eed
Ringo <58303270+Ringo-dev@users.noreply.github.com>
2020-06-26 00:46:11 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 wget https://static.adguard.com/adguardhome/release/AdGuardHome_linux_arm.tar.gz
 tar xvf AdGuardHome_linux_arm.tar.gz
+ｃｄ AdGuardHome
 sudo ./AdGuardHome -s install
+sudo ./AdGuardHome -s start
